ac0f78191c2de0daeed2da6d0f584841dac1d205
Mehdo Abdou <mehdi.abdou96@gmail.com>
2022-10-14 16:16:05 +0200

    projet presque terminer, bug et correctifs a apporter

---
+++ front/src/components/Post/NewPostForm.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { isEmpty, timestampParser } from "../Utils";
+import { NavLink } from "react-router-dom";
+import { addPost, getPosts } from "../../actions/post.actions";
+
+const NewPostForm = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [message, setMessage] = useState("");
+  const [postPicture, setPostPicture] = useState(null);
+  const [file, setFile] = useState();
+  const userData = useSelector((state) => state.userReducer);
+  const errors = useSelector((state) => state.errorReducer.postError);
+  const dispatch = useDispatch();
+
+  const handlePost = () => {
+    if (message || postPicture) {
+      const data = new FormData();
+      data.append("posterId", userData._id);
+      data.append("message", message);
+      if (file) data.append("file", file);
+
+      dispatch(addPost(data));
+      dispatch(getPosts());
+      cancelPost();
+    } else {
+      alert("Veuillez entrer un message");
+    }
+  };
+
+  const cancelPost = () => {
+    setMessage("");
+    setPostPicture("");
+    setFile("");
+  };
+
+  const handlePicture = (e) => {
+    setPostPicture(URL.createObjectURL(e.target.files[0]));
+    setFile(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    if (!isEmpty(userData)) setIsLoading(false);
+  }, [userData]);
+
+  return (
+    <div className="post-container">
+      {isLoading ? (
+        <i className="fas fa-spinner fa-pulse"></i>
+      ) : (
+        <>
+          <NavLink to="/profil">
+            <div className="user-info">
+              <img src={userData.picture} alt="utilisateur connecté" />
+            </div>
+          </NavLink>
+          <div className="post-form">
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Publiez votre humeur du jour!"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+            {message || postPicture ? (
+              <li className="card-container">
+                <div className="card-left">
+                  <img src={userData.picture} alt="utilisateur connecté" />
+                </div>
+                <div className="card-right">
+                  <div className="card-hearder">
+                    <div className="pseudo">
+                      <h3>{userData.pseudo}</h3>
+                    </div>
+                    <span>{timestampParser(Date.now())}</span>
+                  </div>
+                  <div className="content">
+                    <p>{message}</p>
+                    <img src={postPicture} alt="publication d'un utilisateur" />
+                  </div>
+                </div>
+              </li>
+            ) : null}
+            <div className="footer-form">
+              <div className="icon">
+                <>
+                  <img src="./img/icons/picture.svg" alt="icone d'upload de fichier" />
+                  <input
+                  aria-label="upload-picture"
+                    type="file"
+                    id="file-upload"
+                    name="file"
+                    accept=".jpg, .jpeg, .png"
+                    onChange={(e) => handlePicture(e)}
+                  />
+                </>
+              </div>
+              {!isEmpty(errors.format) && <p>{errors.format}</p>}
+              {!isEmpty(errors.maxSize) && <p>{errors.maxSize}</p>}
+              <div className="btn-send">
+                <button className="send" onClick={handlePost}>
+                  Envoyer
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default NewPostForm;
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { UidContext } from "../components/AppContext";
+import NewPostForm from "../components/Post/NewPostForm";
+import Thread from "../components/Thread";
+import Log from "../components/Log";
+import LeftNav from "../components/LeftNav";
 
-const home = () => {
-    return (
-        <div>
-            hello depuis Home
+const Home = () => {
+  const uid = useContext(UidContext);
+  return (
+    <div className="home" role="main" aria-label="Page d'accueil">
+      <LeftNav />
+      <div className="main">
+        <div className="home-header">
+          {uid ? <NewPostForm /> : <Log signin={true} signup={false} />}
         </div>
-    );
+        <Thread />
+      </div>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
