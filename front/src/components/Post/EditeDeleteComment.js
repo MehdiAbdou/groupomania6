6ee540d25ab4c0a001178d4eb370de5ae946cc82
Mehdo Abdou <mehdi.abdou96@gmail.com>
2022-10-21 17:53:08 +0200

    Admin commentaire

--- front/src/components/Post/EditeDeleteComment.js
+++ front/src/components/Post/EditeDeleteComment.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deleteComment, editComment } from "../../actions/post.actions";
 import { UidContext } from "../AppContext";
 
@@ -8,6 +8,7 @@ const EditDeleteComment = ({ comment, postId }) => {
   const [edit, setEdit] = useState(false);
   const [text, setText] = useState("");
   const uid = useContext(UidContext);
+  const userData = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
 
   const handleEdit = (e) => {
@@ -33,12 +34,12 @@ const EditDeleteComment = ({ comment, postId }) => {
 
   return (
     <div className="edit-comment">
-      {isAuthor && edit === false && (
+      {isAuthor && edit === false&& (
         <span onClick={() => setEdit(!edit)}>
           <img src="./img/icons/edit.svg" alt="edit-comment" />
         </span>
       )}
-      {isAuthor && edit && (
+      {((isAuthor && edit) || userData.isAdmin === true) && (
         <form action="" onSubmit={handleEdit} className="edit-comment-form">
           <label htmlFor="text" onClick={() => setEdit(!edit)}>
             Editer
